5729dc25a1b96bcfd094fa2706b40e36e38f2991
sanjar00 <54155922+sanjar00@users.noreply.github.com>
2020-06-10 18:26:39 +0500

    Checking

---
+++ index.php
@@ -0,0 +1,276 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+	<script src="assets_pages/land/js/jquery/jquery-2.1.3.min.js"></script>
+	<title>Silikon qoplamalar</title>
+	<meta name="description" content="Силиконовые крышки" />
+	<meta name="keywords" content="Крышки, силикон, силиконовые крышки" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="theme-color" content="#262626" />
+	<link href="https://fonts.googleapis.com/css?family=Pattaya&amp;subset=cyrillic" rel="stylesheet">
+	<link rel="stylesheet" href="css/slick.css" type="text/css" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<link rel="stylesheet" href="OwlCarousel2/dist/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="OwlCarousel2/dist/assets/owl.theme.default.min.css">
+</head>
+
+<body>
+	<div class="container">
+		<section class="title-block clearfix">
+			<span>Silikon qoplamalar</span>
+			<a href="#zakaz">Buyurmoq</a>
+		</section>
+		<div class="top">
+			<img src="Images/2.jpg" alt="">
+		</div>
+		<div class="discount rop">
+			<div class="dis_left">
+				<div class="text">Eski narx</div>
+				<span class="sub_text"><span>535.000</span> so'm</span>
+			</div>
+			<div class="dis_right">
+				<div class="text">Chegirmalangan narx</div>
+				<span class="sub_text">350.000 so'm</span>
+			</div>
+		</div>
+		<section class="offer rop">
+			<div class="text">Targ'ibot oxirigacha:</div>
+			<div class="timer-action"></div>
+			<a href="#zakaz" class="click">
+				<button class="button">Buyurtma berish</button>
+			</a>
+			<div class="remaining">Chegirma bilan <i>12</i> qoplam qoldi</div>
+		</section>
+		
+		<section class="capability">
+			<div class="about_title">
+				<span class="hud">Hajmlar</span>
+			</div>
+			<div class="cap_inner">
+				<img src="Images/second_bg.jpg" alt="sizes">
+			</div>
+		</section>
+		
+		<div class="about_title">
+			Qopqoq <span class="hud">Afzalliklari</span>
+		</div>
+		<section class="benefits">
+
+			<div class="ben_inner">
+				<div class="ben_block 1">
+					<img class="ben_left" src="png/done.png" alt="">
+					<div class="ben_right">
+						<div class="ben_title">6 HAJMI:</div>
+						<div class="ben_text">Har bir mahsulot 6 o'lchamlarini o'z ichiga oladi</div>
+					</div>
+				</div>
+				<div class="ben_block 2">
+					<img class="ben_left" src="png/done.png" alt="">
+					<div class="ben_right">
+						<div class="ben_title">ISSIQLIKKA CHIDAMLI:</div>
+						<div class="ben_text">Mikroto'lqinli pechda, idishlarni yuvish mashinasida, muzlatgichda xavfsiz foydalanishi mumkin.</div>
+					</div>
+				</div>
+				<div class="ben_block 3">
+					<img class="ben_left" src="png/done.png" alt="">
+					<div class="ben_right">
+						<div class="ben_title">QAYTA FOYDALANADIGAN VA BARDOSHLI:</div>
+						<div class="ben_text">Qalin silikon bardoshli va yirtilmaydi yoki buzilmaydi.</div>
+					</div>
+				</div>
+				<div class="ben_block 4">
+					<img class="ben_left" src="png/done.png" alt="">
+					<div class="ben_right">
+						<div class="ben_title">MUVOFIQLIGI:</div>
+						<div class="ben_text">Konteynerlarning ko'p o'lchamlari va shakllari bilan mos keladi.</div>
+					</div>
+				</div>
+				<div class="ben_block 4">
+					<img class="ben_left" src="png/done.png" alt="">
+					<div class="ben_right">
+						<div class="ben_title">OZIQ-OVQATLARNI YANGI SAQLAYDI:</div>
+						<div class="ben_text">Havo o'tkazmaydigan, oziq-ovqatlarni uzoqroq saqlaydi va to'kilishning oldini oladi.</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<section class="about">
+			<div class="about_title">
+				<span class="hud">Oson</span> ishlatilinishi
+			</div>
+			<iframe width="480" height="270" src="https://www.youtube.com/embed/w8hXMFmkOHU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			<div class="text_cover">
+			<p class="about_text">
+				Ular shisha idishni, alyuminiy panani, keramik tovoqni va hatto plastik idishni zudlik bilan saqlashga aylantiradigan <span class="hud">silikon qopqoqlardir</span>.
+			</p>
+			</div>
+		</section>
+
+
+		
+		<div class="owl-carousel slide-one">
+			<div class="slider_img"><img src="Images/first_bg.jpg" alt=""></div>
+			<div class="slider_img"><img src="Images/1.jpg" alt=""></div>
+			<div class="slider_img"><img src="Images/png/example_1.png" alt=""></div>
+		</div>
+
+		<div class="about_title" style="font-size: 24px;">
+			<span class="hud">Nega</span> silikon qoplamlar?
+		</div>
+
+		<section class="items">
+			<div class="items_inner 1">
+				<img src="Images/png/why.jpeg" alt="" class="items_left on">
+					<p class="items_text">Ular shisha idishni, alyuminiy panani, keramik tovoqni va hatto plastik idishni darhol saqluvga aylantiradi.</p>
+			</div>
+			<div class="items_inner tw">
+                <img src="Images/png/why_1.jpeg" alt="" class="items_right">
+					<p class="items_text">Qoplamlar 6 xil kattaliklardan, kichik kosadan katta chuqur kosagacha. Agar Siz oziq-ovqatni katta miqdorda saqlaydigan bo'lsangiz, ular, ayniqsa, foydali.</p>
+				
+			</div>
+			<div class="items_inner 3">
+				<img src="Images/png/why_2.jpeg" alt="" class="items_left th">
+					<p class="items_text">Ular ekologik toza, yopishqoq plyonkalarni qayta-qayta sotib olish zaruratini bartaraf etadi.</p>
+			</div>
+			<div class="items_inner 4">
+                <img src="Images/png/why_3.jpeg" alt="" class="items_right fo">
+					<p class="items_text">Mikroto'lqinli pechda 232 ° C / 450 F gacha issiq va issiqqa chidamli, shuning uchun siz tezda qizdirib va lazzatli qoldiqlardan lazzatlanishingiz mumkin.</p>
+				
+			</div>
+		</section>
+
+
+
+
+		<div class="about_title" style="font-size: 24px;">
+			<span class="hud">Mijozlarning</span> mulohazalari
+		</div>
+
+		<div class="owl-carousel slide-two">
+			<div class="review">
+				<div class="img_cov">
+					<div class="rev_img"><img src="Images/png/face.png" alt=""></div>
+				</div>
+				<h3 class="rev_name">Otabek Ganiyev, Jizzah <span>sh.</span></h3>
+				<div class="breaker"></div>
+				<p>Men buni juda tavsiya qilaman! Ular, albatta, plitalar, idishlar, qozonlarda turishadi. Agar siz qoplamaning to'g'ri hajmini tanlasangiz, unda hech narsa oqmaydi. Rangi yoqimli. Hidsiz. O'lchamlarning qat'iy tanlovi. Ko'rsatmalarda uni hamma joyda ishlatish mumkinligi aytilgan.</p>
+			</div>
+			<div class="review">
+				<div class="img_cov">
+					<div class="rev_img"><img src="Images/png/face_2.png" alt=""></div>
+				</div>
+				<h3 class="rev_name">Светлана Сураева, Toshkent <span>sh.</span></h3>
+				<div class="breaker"></div>
+				<p>Zo'r ko'p funktsiyali qopqoqlar, salat idishlari, idishlar, stakanlarga mos keladi. Bolali oilalar uchun juda mos keladi, bola ovqatlanmagach, siz qopqoqni yopishingiz va muzlatgichga qo'yishingiz mumkin.</p>
+				<div class="subt_text">(tarjima qilingan)</div>
+			</div>
+		</div>
+
+		<div class="about_title last" style="font-size: 24px;">
+			Etkazib berish va to'lash
+		</div>
+		<section class="offerist">
+			<div class="ben_block hop">
+				<img class="offer_img" src="png/delivery.png" alt="">
+				<div class="ben_right bot">
+					<div class="ben_title">TEZKOR YETKAZIB BERISH</div>
+					<div class="ben_text">1-3 kun ichida O'zbekistonda etkazib berish</div>
+				</div>
+			</div>
+			<div class="ben_block hop">
+				<img class="offer_img .sec" src="png/payment.png" alt="">
+				<div class="ben_right bot">
+					<div class="ben_title">ERTA TO'LO'VSIZ</div>
+					<div class="ben_text">Siz faqat tovarlarni qo'lda olganingizda to'laysiz</div>
+				</div>
+			</div>
+			<div class="ben_block hop">
+				<img class="offer_img .thr" src="png/guarantee.png" alt="">
+				<div class="ben_right bot">
+					<div class="ben_title">1 YIL KAFOLAT</div>
+					<div class="ben_text">Biz har doim yuborishdan oldin tovarlarni tekshiramiz va 100% sifatiga kafolat beramiz.</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="about_title" style="font-size: 24px;">
+			<span class="hud">Silikon</span> Qoplamalar
+		</div>
+		<div class="top rtop">
+			<img src="Images/2.jpg" alt="">
+		</div>
+		<div class="discount">
+			<div class="dis_left">
+				<div class="text">Eski narx</div>
+				<span class="sub_text"><span>535.000</span> so'm</span>
+			</div>
+			<div class="dis_right">
+				<div class="text">Chegirmalangan narx</div>
+				<span class="sub_text">350.000 so'm</span>
+			</div>
+		</div>
+
+		<section class="offer">
+			<div class="text">Targ'ibot oxirigacha:</div>
+			<div class="timer-action"></div>
+			<form id="zakaz" class="main-order-form" action="send.php" method="post">
+				<div><input type="text" name="name" placeholder="To'liq ismingizni kiriting" required=""></div>
+				<div><input type="tel" name="phone" placeholder="Telefon raqamni kiriting" required=""></div>
+				<div><select name="amount" required>
+						<option selected disabled value=''>Miqdorni tanlang:</option>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+					</select>
+				</div>
+				<div><button class="button">Buyurish</button></div>
+				<input type="hidden" name="l" value="7aba516aed6850175d71d1222d5a7f5dba77fa2e" />
+				<input type="hidden" name="86e3bde814f0301231fccea403de63612c103010" value="MzFiYzlkNjhmZTE2NjAzZmZlOGIzODk4Y2Y3MTlhNWIyNjU=" />
+			</form>
+			<div class="remaining">Chegirma bilan <i>12ta</i> qoplam qoldi</div>
+
+		</section>
+		<section class="copyright">
+			<a style="color: inherit;" href="politics.html" target="_blank">Maxfiylik&nbsp;siyosati</a>&emsp;<a style="color: inherit;" href="agreement.html" target="_blank">Foydalanuvchi&nbsp;shartnomasi</a>
+		</section>
+
+	</div>
+
+
+
+	<script>
+		$(document).ready(function() {
+			$(".slide-one").owlCarousel({
+				loop: true,
+				autoplay: true,
+				autoplayTimeout: 5000,
+				autoplayHoverPause: false,
+				items: 1,
+				margin: 10,
+				autoHeight: true,
+				smartSpeed: 3000
+			});
+			$(".slide-two").owlCarousel({
+				loop: true,
+				autoplay: true,
+				autoplayTimeout: 5000,
+				autoplayHoverPause: false,
+				items: 1,
+				margin: 10,
+				autoHeight: true,
+				smartSpeed: 2000
+			});
+		});
+
+	</script>
+	<script type="text/javascript" src="js/slick.min.js"></script>
+	<script type="text/javascript" src="js/init.js"></script>
+	<script type="text/javascript" src="OwlCarousel2/dist/owl.carousel.min.js"></script>
+
+
+</body>
