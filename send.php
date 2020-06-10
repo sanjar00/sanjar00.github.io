807baaea8e44703c258eb2f52d7b7e58aed40efb
sanjar00 <54155922+sanjar00@users.noreply.github.com>
2020-06-10 21:28:46 +0500

    intex

--- send.php
+++ send.php
@@ -9,7 +9,7 @@
 <style>
 .block-1{
     color: #fff;
-font-size: 30px;
+	font-size: 30px;
     border: 0;
     background: #f09433;
     background: -moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
@@ -21,20 +21,41 @@ font-size: 30px;
 
 <?php 
 
-$username = strip_tags($_POST['name']);   // сохраняем в переменную данные полученные из поля c именем
-$usertel = strip_tags($_POST['phone']); // сохраняем в переменную данные полученные из поля c телефонным номером
-$amount = strip_tags($_POST['amount']);
-
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+ 
+//Переменная $name,$phone, $mail получает данные при помощи метода POST из формы
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+ 
+//в переменную $token нужно вставить токен, который нам прислал @botFather
 $token = "1124908145:AAGRZQy_YJ4kTVDmKdcQH57rZTWN0GwGaDg";
-$chatid = "818962424";
-$mess = "
-Name: $username
-Phone: $usertel
-Amount: $amount
-";
-$tbot = file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chatid."&text=".urlencode($mess));
-	
-
+ 
+//нужна вставить chat_id (Как получить chad id, читайте ниже)
+$chat_id = "818962424";
+ 
+//Далее создаем переменную, в которую помещаем PHP массив
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone
+);
+ 
+//При помощи цикла перебираем массив и помещаем переменную $txt текст из массива $arr
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+ 
+//Осуществляется отправка данных в переменной $sendToTelegram
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+ 
+//Если сообщение отправлено, напишет "Thank you", если нет - "Error"
+if ($sendToTelegram) {
+  echo "Thank you";
+} else {
+  echo "Error";
+}
+?>
 
 </body>
 </html>
+
