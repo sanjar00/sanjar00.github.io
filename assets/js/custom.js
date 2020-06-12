d4c21e2428ec5676f19d67b6fd8c91d90cec254a
sanjar00 <54155922+sanjar00@users.noreply.github.com>
2020-06-12 21:04:00 +0500

    delete

--- assets/js/custom.js
+++
@@ -1,6 +0,0 @@
-/*==================================================================================
-    Custom JS (Any custom js code you want to apply should be defined here).
-====================================================================================*/
-
-var d = new Date();
-document.getElementById("year").innerHTML = d.getFullYear();
